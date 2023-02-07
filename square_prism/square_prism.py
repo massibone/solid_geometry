bf92500177e13898be8d9c0b3a0ff5fbfd80c876
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-02-07 08:07:05 +0100

    Update square_prism.py

--- square_prism/square_prism.py
+++ square_prism/square_prism.py
@@ -1,29 +1,4 @@
-'''A prism is an important member of the polyhedron family that
- has congruent polygons at the base and top.
- The other faces of a prism are parallelograms are called lateral faces. 
- It means that a prism does not have a curved face. 
- A prism has the same cross-section all along its length. 
- The prisms are named depending upon their cross-sections. 
- The most common example of a prism is a metallic nut.
-'''
-'''
-There are different types of prisms based on the shape of the base of a prism: 
 
-Triangular prisms,
-Square prisms, 
-Rectangular prisms, 
-Pentagonal prisms, 
-Hexagonal prisms, etc.
-Trapezoidal prism
-A square prism is a prism that has a square base.
-All the cubes are actually square prisms
-
-
-
-
-
-
-'''
 # Import libraries
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
