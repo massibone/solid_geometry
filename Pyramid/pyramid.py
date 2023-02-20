5a62c1351105918b40af5117a1ed3738661aed54
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-02-20 08:00:58 +0100

    Add files via upload

---
+++ Pyramid/pyramid.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# vertices of a pyramid
+v = np.array([[-1, -1, -1], [1, -1, -1], [1, 1, -1],  [-1, 1, -1], [0, 0, 1]])
+ax.scatter3D(v[:, 0], v[:, 1], v[:, 2])
+
+# generate list of sides' polygons of our pyramid
+verts = [ [v[0],v[1],v[4]], [v[0],v[3],v[4]],
+ [v[2],v[1],v[4]], [v[2],v[3],v[4]], [v[0],v[1],v[2],v[3]]]
+
+# plot sides
+ax.add_collection3d(Poly3DCollection(verts, 
+ facecolors='cyan', linewidths=1, edgecolors='r', alpha=.25))
+
+plt.show()
