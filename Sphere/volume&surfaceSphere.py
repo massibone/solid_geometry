56a5e5234d11da13aef929e6a363bedb175bf6f9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-02-22 09:41:14 +0100

    Add files via upload

---
+++ Sphere/volume&surfaceSphere.py
@@ -0,0 +1,7 @@
+pi=3.14
+radian = float(input("Enter The Radius of Sphere :"))
+sur_area = 4 * pi * radian **2
+volume = (4/3) * (pi * radian ** 3)
+print("Surface Area is: ", sur_area)
+print("Volume is: ", volume)
+
